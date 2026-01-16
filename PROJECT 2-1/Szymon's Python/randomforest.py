3032dd66c88b43216afe56054b1e10d0aed24e49
Szymon Milewski <mszymoon@gmail.com>
2026-01-16 13:24:25 +0100

    RandomForestClassifier

---
+++ PROJECT 2-1/Szymon's Python/randomforest.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+filename = ''
+data = pd.read_csv(filename)
+
+def forest(data):
+    y = data['output']
+    X = data.drop(['output'], axis=1)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+
+    n_estimators = [int(x) for x in np.linspace(start = 10, stop = 200, num=10)]
+    max_features = ['sqrt', 'int', None]
+    max_depth = [2, 4, 6, 8]
+    min_samples_split = [2, 4, 5]
+    min_samples_leaf = [1, 2, 3]
+    bootstrap = [True, False]
+
+
+    parameter_grid = {'n_estimators': n_estimators,
+                    'max_features': max_features,
+                    'max_depth': max_depth,
+                    'min_samples_split': min_samples_split,
+                    'min_samples_leaf': min_samples_leaf,
+                    'bootstrap': bootstrap}
+
+    baseForest = RandomForestClassifier()
+
+    gridForest = GridSearchCV(estimator=baseForest, param_grid=parameter_grid, cv=8, verbose=2, n_jobs=4)
+
+    gridForest.fit(X_train, Y_train)
+
+    return gridForest.best_estimator_
+
+
+
+
